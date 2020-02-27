1875d55d7b6d8d926ea51042db091d36da8cc654
Unknown <jenny9993@hotmail.com>
2020-02-26 20:04:27 -0800

    cleaned part

--- DC2.py
+++ DC2.py
@@ -4,6 +4,7 @@ import nltk
 from nltk.corpus import stopwords
 import string
 
+
 # df = pd.read_csv(r'C:\Users\Alexis Hale\PycharmProjects\NLP-DC2\spam_notriplecommas.csv')   #read the csv file (put 'r' before the path string to address any special characters in the path, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
 # print(df)
 
@@ -12,19 +13,25 @@ v1 = []
 v2 = []
 
 
-with open('spam.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',', lineterminator=',,,')
+with open('spam.csv', encoding="ISO-8859-1") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
+            v1.append(row["v1"])
+            v2.append(row["v2"])
             line_count += 1
         else:
-            v1.append(row[0])
-            v2.append(row[1])
-            print(row[0], row[1])
             line_count += 1
+            v1.append(row["v1"])
+            v2.append(row["v2"])
+            #if you uncommnet then it will print out the messages
+            #print(row["v1"],row["v2"])
+
 print(f'Processed {line_count} lines.')
+
+#print(v2[0:5])
 
 def clean_text(text):
     remove_punc = [char for char in text if char not in string.punctuation]
@@ -32,7 +39,9 @@ def clean_text(text):
     clean_words = [word for word in remove_punc.split() if word.lower() not in stopwords.words('english')]
     return clean_words
 
-print(clean_text(v2[1]))
+print(clean_text(v2[2]))
+
+
 
 
 # input_file = csv.DictReader(open("spam.csv"))
