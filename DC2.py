c1021750bcd92b8cb1133b8ec1109fd0935d313a
Unknown <jenny9993@hotmail.com>
2020-03-01 09:02:55 -0800

    clean up and change the lr to multinomial nb

--- DC2.py
+++ DC2.py
@@ -1,23 +1,11 @@
-import pandas as pd
 import csv
-import nltk
 from nltk.corpus import stopwords
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn import svm
-from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 
-# df = pd.read_csv(r'C:\Users\Alexis Hale\PycharmProjects\NLP-DC2\spam_notriplecommas.csv')   #read the csv file (put 'r' before the path string to address any special characters in the path, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
-# print(df)
-
-#store the spam or ham label and actual content
-from sklearn.model_selection import train_test_split
-
-v1 = []
-v2 = []
+label = []
+text = []
 
 with open('spam.csv', encoding="ISO-8859-1") as csv_file:
     csv_reader = csv.DictReader(csv_file)
@@ -25,19 +13,18 @@ with open('spam.csv', encoding="ISO-8859-1") as csv_file:
     for row in csv_reader:
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
-            v1.append(row["v1"])
-            v2.append(row["v2"])
+            label .append(row["v1"])
+            text.append(row["v2"])
             line_count += 1
         else:
             line_count += 1
-            v1.append(row["v1"])
-            v2.append(row["v2"])
+            label .append(row["v1"])
+            text.append(row["v2"])
             #if you uncommnet then it will print out the messages
             #print(row["v1"],row["v2"])
 
-print(f'Processed {line_count} lines.')
+#print(f'Processed {line_count} lines.')
 
-#print(v2[0:5])
 
 def clean_text(text):
     remove_punc = [char for char in text if char not in string.punctuation]
@@ -45,46 +32,59 @@ def clean_text(text):
     clean_words = [word for word in remove_punc.split() if word.lower() not in stopwords.words('english')]
     return clean_words
 
-#print(clean_text(v2[2]))
-# to use a logistic regression, we first convert all the data in to
-#vectorizer = TfidfVectorizer("english")
-#message = vectorizer.fit_transform(v2)
-#print(message)
+#vecotrize the inputs
+vectorizer = TfidfVectorizer("english")
+message = vectorizer.fit_transform(text)
+
 
 # this does the spliting for the training and testing data
-#m_train, m_test, sns_train, sns_test = train_test_split(message,
-#                                                        v1, test_size=0.3, random_state=20)
-x_train, x_test, y_train, y_test = train_test_split(v2, v1, test_size = 0.2, random_state = 1)
+x_train, x_test, y_train, y_test = train_test_split(message, label, test_size = 0.2, random_state = 1)
 
 
-vectorizer = TfidfVectorizer()
-x_train = vectorizer.fit_transform(x_train)
+# MultinomialNB Classifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report,confusion_matrix, accuracy_score
+classifier = MultinomialNB()
+classifier.fit(x_train, y_train)
+
+nvb_pred = classifier.predict(x_test)
+print(classification_report(y_test ,nvb_pred ))
+print('Confusion Matrix: \n',confusion_matrix(y_test,nvb_pred))
+print('Accuracy: ', accuracy_score(y_test,nvb_pred))
+
+print()
+print()
+
+#Support Vector Machine
+from sklearn import svm
+svm = svm.SVC(C=1000)
+svm.fit(x_train, y_train)
+
+svm_pred = svm.predict(x_test)
+print(classification_report(y_test, svm_pred ))
+print("Confusion Matrix: \n'", confusion_matrix(y_test, svm_pred))
+print('Accuracy: ', accuracy_score(y_test, svm_pred))
 
 
-#logistic Regression
+def pred(msg):
+    msg = vectorizer.transform([msg])
+    prediction = svm.predict(msg)
+    return prediction[0]
+
+for i in range (11):
+    print (text[i],pred(text[i]))
+
+
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 '''
+#logistic Regression
+
 Spam_model = LogisticRegression(solver='liblinear', penalty='l1')
 Spam_model.fit(x_train, y_train)
 pred = Spam_model.predict(x_test)
 print(accuracy_score(y_test,pred))
 confusion_matrix_lr = pd.crosstab(y_test, pred)
-print(confusion_matrix_lr)
+print("Confusion Matrix: \n'",confusion_matrix_lr)
 '''
-
-#Support Vector Machine
-svm = svm.SVC(C=1000)
-svm.fit(x_train, y_train)
-
-X_test = vectorizer.transform(x_test)
-y_pred = svm.predict(X_test)
-print(confusion_matrix(y_test, y_pred))
-
-
-
-# input_file = csv.DictReader(open("spam.csv"))
-
-# for row in input_file:
-#     print(row)
-# else:
-#     print("The file does not have any content")
-
